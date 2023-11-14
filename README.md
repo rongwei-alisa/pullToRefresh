af276b8b41714bfd9a9ce805a40657be537f19f3
rongwei <rongwei_alisa@hotmail.com>
2023-11-14 15:34:31 +0800

    feat: 修改文件目录

---
+++ README.md
@@ -0,0 +1,2 @@
+下拉刷新 -> pullToRefresh
+上拉加载 -> pullUpLoading
---
+++ pullUpLoading/index.js
@@ -0,0 +1,15 @@
+      // const NUM = 100;
+    // const content = document.getElementById('content');
+    // for(let i = 0; i < NUM; i++) {
+    //     const div = document.createElement('div');
+    //     div.innerText = i;
+    //     content.appendChild(div);
+    // }
+
+    // const scrollTop = content.scrollTop,
+    //     scrollHeight = content.scrollHeight,
+    //     clientHeight = document.clientHeight;
+
+    //     console.log({scrollTop,scrollHeight,clientHeight})
+    
+
